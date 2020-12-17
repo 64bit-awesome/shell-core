6c350c4a99b8a7de501ee60bdfd39d93d1b7f553
Jordy Araujo <jordy.araujo@hotmail.com>
2020-12-17 11:26:06 -0500

    Add pipe support
    Process executable statements from pipe-chain in left-most order:
    
                        cmd1 | cmd2 | cmd3
                         1      2      3
    
    Use pipe to redirect output from previous command to the next:
    
    If no pipes are present:    no need to redirect output
    If pipes are present:       redirect output to the input of next
    If at last statement:       set output to STDOUT

--- shell-core.c
+++ shell-core.c
@@ -25,6 +25,7 @@ typedef struct // holds information about the current command-line.
     char* tokens[MAX_TOKENS]; // ->s to the tokens in cmdline.
 } CmdLine;
 
+void spawn(CmdLine* cmdline, int* fdd, int pipes, int executableIndex);
 void tokenize(CmdLine* cmdline);
 void execute(CmdLine* cmdline);
 
@@ -91,6 +92,40 @@ void execute(CmdLine* cmdline)
         free(cmdline);
         exit(0); // clean exit.
     }
+    
+    int i; // index of the current token being processed.
+    int pipes = 0; // number of pipes processed.
+    int executableIndex = 0; // index of the executable program in cmdline.
+    int lastPipeIndex = -1; // index of the last pipe processed.
+
+    int fdd = STDIN_FILENO; // copy of one end of pipe.
+
+    for(i = 0; i < cmdline->ntokens; i++)
+    {
+        if(pipes != 0 && pipes == cmdline->npipes) // last executable:
+        {
+            executableIndex = lastPipeIndex + 1; // maybe can just = i ?
+            spawn(cmdline, &fdd, pipes, executableIndex);
+            
+            i = cmdline->ntokens; // we're done; that was the last command to execute.
+        }
+        else if(!strcmp(cmdline->tokens[i], "|")) // more executables:
+        {
+            lastPipeIndex = i;
+
+            spawn(cmdline, &fdd, pipes, executableIndex);
+            
+            // allow first stmt in pipe-chain to execute:
+            pipes++;
+            executableIndex = lastPipeIndex + 1;
+        }
+        else if (cmdline->npipes == 0) // no pipes:
+        {
+            spawn(cmdline, &fdd, pipes, executableIndex);
+            i = cmdline->ntokens; // we're done; that was the last command to execute.
+        }
+        
+    }
 }
 
 /**
@@ -102,8 +137,11 @@ void tokenize(CmdLine* cmdline)
     int count = 0; // number of tokens found.
     int pipeCount = 0; // number of pipes found.
 
-    if((cmdline->tokens[0] = strtok(cmdline->line, "\n\t ")) == NULL) 
+    if((cmdline->tokens[0] = strtok(cmdline->line, "\n\t ")) == NULL)
+    {
         cmdline->ntokens = 0; // no tokens found.
+        return;
+    }
 
     // tokenize by newlines, tabs, and spaces:
     while((cmdline->tokens[++count] = strtok(NULL, "\n\t ")) != NULL)
@@ -121,4 +159,84 @@ void tokenize(CmdLine* cmdline)
     
     cmdline->ntokens = count; // store number of tokens found.
     cmdline->npipes = pipeCount; // store number of pipes found.
+}
+
+void spawn(CmdLine* cmdline, int* fdd, int pipes, int executableIndex)
+{
+    int stdOutFd = STDOUT_FILENO; // the file descriptor to be used as stdout in child processes.
+    
+    int pid; // the process id of the current process.
+    int fd[2]; // ->s to pipe ends.
+    
+    // attempt to create a pipe:
+    if(pipe(fd) < 0)
+    {   // if here, pipe failed:
+        fprintf(stderr, "internal-error: \n\t");
+        fprintf(stderr, "pipe-creation: failed. \n");
+
+        free(cmdline);
+        exit(-1);
+    }
+
+    int nArgsToChild = cmdline->ntokens + 1;
+    char* argsToChild[nArgsToChild]; // ->s to arguments for child process.
+    int j; // index of current token in child argument array. 
+
+    // initialize array:
+    for(j = 0; j < nArgsToChild; j++)
+    {
+        argsToChild[j] = NULL;
+    }
+
+    // copy over arguments:
+    for(j = 0; (executableIndex + j) < cmdline->ntokens; j++)
+    {
+        if(!strcmp(cmdline->tokens[executableIndex + j], "|") 
+            || !strcmp(cmdline->tokens[executableIndex + j], ">")
+            || !strcmp(cmdline->tokens[executableIndex + j], ">>")
+            || !strcmp(cmdline->tokens[executableIndex + j], " "))
+        {
+            j = cmdline->ntokens; // stop copying; everything else is irrelevant.
+        }
+        else
+        {
+            argsToChild[j] = malloc(strlen(cmdline->tokens[executableIndex + j]) + 1);
+            strcpy(argsToChild[j], cmdline->tokens[executableIndex + j]);
+        }
+    }
+
+    // attempt to fork a child process:
+    if((pid = fork()) < 0)
+    {   // if here, fork failed:
+        fprintf(stderr, "internal-error: \n\t");
+        fprintf(stderr, "fork: failed. \n");
+
+        free(cmdline); // TODO: fix memory leak in argsToChild.
+        exit(-1);
+    }
+    else if(pid == 0) // child process:
+    {
+        if(cmdline->npipes > 0 && pipes != cmdline->npipes) // && outputfile != null
+        {
+            stdOutFd = fd[1];
+        }
+        
+        dup2(*fdd, STDIN_FILENO); // set the input.
+        dup2(stdOutFd, STDOUT_FILENO); // set the output.
+
+        close(fd[0]);
+        execvp(cmdline->tokens[executableIndex], argsToChild); // replace image in child process.
+
+        // if here, something has gone horribly wrong:
+        fprintf(stderr, "invalid-executable:\n\tunable to execute %s.\n\tmake sure it is in your path.\n", cmdline->tokens[executableIndex]);
+            
+        free(cmdline); // TODO: fix memory leak in argsToChild.
+        exit(-1);
+    }
+    else // parent:
+    {
+        wait(NULL);		
+        close(fd[1]);
+        *fdd = fd[0]; // save output from previous stmt in pipe-chain.
+    }
 }
